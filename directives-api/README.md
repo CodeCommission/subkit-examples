f8930ca89c5297c2da315e7c33478da9e6a999b2
Mike Bild <mike.bild@googlemail.com>
2017-10-30 06:21:06 +0100

    Upgrade SubKit

--- directives-api/README.md
+++ directives-api/README.md
@@ -1,5 +1,7 @@
 # SubKit GraphQL Directives API
 
+> Demonstrate how Server-Side and Client-Side directives work.
+
 [Live Example](https://subkit-directives.cloud.dropstack.run/graphql)
 
 ```graphql
@@ -7,7 +9,7 @@ query allItems {
   items @mock(value: [{id: "MikeBild", email: "mike.bild@gmail.com"}, {id: "SubKit"}]) {
     id @toLowerCase
     email
-    profile @fetchJSON(url: "https://de.gravatar.com/${id}.json", jsonQuery: "entry[0]", timeout: 1000) {
+    profile {
       displayName
       description @toFormatString(template: "${displayName} (${aboutMe})", parent: true)
       thumbnailUrl
--- directives-api/graphql.js
+++ directives-api/graphql.js
@@ -1,26 +1,34 @@
-const {GraphQLString, GraphQLBoolean, GraphQLNonNull} = require('graphql')
-const {DirectiveLocation} = require('graphql/type/directives')
-const format = require('es6-template-strings')
+const { GraphQLString, GraphQLBoolean, GraphQLNonNull } = require("graphql");
+const { DirectiveLocation } = require("graphql/type/directives");
+const format = require("es6-template-strings");
 
-export const resolvers = {
-}
+export const resolvers = {};
 
-export const loaders = {
-}
+export const loaders = {};
 
-export const channels = {
-}
+export const channels = {};
 
 export const directives = {
   toLowerCase: {
-    description: 'Transform result to lowercase.',
+    description: "Transform result to lowercase.",
     locations: [DirectiveLocation.FIELD],
-    resolve: (resolve, parent, args, ctx, info) => resolve().then(result => result.toLowerCase()),
+    resolve: (resolve, parent, args, ctx, info) =>
+      resolve().then(result => result.toLowerCase())
   },
   toFormatString: {
-    description: 'Transform result to ES6 template string.',
+    description: "Transform result to ES6 template string.",
     locations: [DirectiveLocation.FIELD],
-    args: {template: {type: new GraphQLNonNull(GraphQLString)}, parent: {type: GraphQLBoolean}},
-    resolve: (resolve, parent, args, ctx, info) => resolve().then(result => format(args.template, args.parent ? parent : {[`${info.fieldName}`]: result}, {partial: true})),
-  },
-}
+    args: {
+      template: { type: new GraphQLNonNull(GraphQLString) },
+      parent: { type: GraphQLBoolean }
+    },
+    resolve: (resolve, parent, args, ctx, info) =>
+      resolve().then(result =>
+        format(
+          args.template,
+          args.parent ? parent : { [`${info.fieldName}`]: result },
+          { partial: true }
+        )
+      )
+  }
+};
